c80ad88d58b492f59eccd78de0aa9bf33dd3938a
matthewshimingwei <1945402109@qq.com>
2019-11-12 23:07:06 +0000

    fucking tutorial

---
+++ tutorial4/scrableGamePsedoCode.s
@@ -0,0 +1,34 @@
+// time complexity of this algorithm is linear 
+void scrabbleLikeGame()
+{
+    char str[] = value;
+    int []value =values;
+    char *pointer = str;
+    int #pointer2 =values;
+    int []wordCount ; LDR Rx = 0x400000000
+    while(pointer!=NULL) 
+    {
+        int distance = *p-'A';
+        count = Memory.wordCount[RX+distance];first load and then assignment to count 
+        count ++;
+        Memory.word[distance] = count; STRB
+        p++
+    }
+    LDR R0,=0 ;sum 
+    int index = 0;
+    int value = 0;
+    while(index<26)
+    {//need load a value for Memory
+        if(Memory.count[index] >0)
+        {
+            address of values += index
+            LDR wordScore ,[address]
+            value =  Memory.count[index] *wordScore;
+            adddress of values -= index            //recover
+            sum+=value;
+        }
+        index ++;
+    }
+}
+testWord DCB    "BANANA",0
+values  DCB 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26
